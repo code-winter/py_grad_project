e84c10be19c548d7ae64254820c89208c98c903e
Dmitry Popov <ant1h4t3rz@gmail.com>
2021-10-18 16:38:27 +0900

    Added Google Drive to upload functionality

---
+++ gdrive.py
@@ -0,0 +1,125 @@
+from __future__ import print_function
+import os
+import json
+from googleapiclient.discovery import build
+from google_auth_oauthlib.flow import InstalledAppFlow
+from google.auth.transport.requests import Request
+from google.oauth2.credentials import Credentials
+import requests
+from urllib import request
+from googleapiclient.http import MediaFileUpload
+
+
+def delete_files(file_dict):
+    """
+    Deletes local files after the upload to G.Drive
+
+    :param file_dict: dict with file names
+    :return:
+    """
+    directory = os.getcwd()
+    for photo, data in file_dict.items():
+        filename = data['filename']
+        path = os.path.join(directory, filename)
+        os.remove(path)
+
+
+def save_files(file_dict):
+    """
+    Saves photos from URIs into local folder for fruther upload to G.Drive
+
+    :param file_dict: dict with URIs and sizes
+    :return: changed dict with file names
+    """
+    result = dict()
+    pos = 0
+    for photo, data in file_dict.items():
+        temp = dict()
+        filename = str(data['likes']) + '.jpg'
+        url = data['photo'][-1]
+        if not os.path.isfile(filename):
+            with open(filename, 'wb') as img:
+                image = request.urlopen(url).read()
+                img.write(image)
+                temp['filename'] = filename
+                temp['size'] = data['photo'][0]
+                result[pos] = temp
+                pos += 1
+        else:
+            filename = f'{str(data["likes"])}_{data["date"]}.jpg'
+            with open(filename, 'wb') as img:
+                image = request.urlopen(url).read()
+                img.write(image)
+                temp['filename'] = filename
+                temp['size'] = data['photo'][0]
+                result[pos] = temp
+                pos += 1
+    return result
+
+
+class GDrive:
+    SCOPES = ['https://www.googleapis.com/auth/drive.file']
+    url = "https://www.googleapis.com/upload/drive/v3/files"
+
+    def auth_gdrive(self):
+        """
+        This authenticates user for Drive API
+
+        :return: credentials object
+        """
+        creds = None
+        # The file token.json stores the user's access and refresh tokens, and is
+        # created automatically when the authorization flow completes for the first
+        # time.
+        if os.path.exists('token.json'):
+            creds = Credentials.from_authorized_user_file('token.json', self.SCOPES)
+        # If there are no (valid) credentials available, let the user log in.
+        if not creds or not creds.valid:
+            if creds and creds.expired and creds.refresh_token:
+                creds.refresh(Request())
+            else:
+                flow = InstalledAppFlow.from_client_secrets_file(
+                    'credentials.json', self.SCOPES)
+                creds = flow.run_local_server(port=0)
+            # Save the credentials for the next run
+            with open('token.json', 'w') as token:
+                token.write(creds.to_json())
+        return creds
+
+    def _upload_to_gdrive(self, jpg_dict):
+        """
+        Performs a upload to Google Drive into a folder named "API_folder"
+
+        :param jpg_dict: dict with URIs for the upload
+        :return: dict with local file names
+        """
+        data = dict()
+        counter = 0
+        creds = self.auth_gdrive()
+        service_fld = build('drive', 'v3', credentials=creds)
+        folder_metadata = {
+            "name": "API_folder",
+            "mimeType": "application/vnd.google-apps.folder"
+        }
+        file = service_fld.files().create(body=folder_metadata, fields="id").execute()
+        folder_id = file.get('id')
+        service = build('drive', 'v3', credentials=creds)
+        info_dict = save_files(jpg_dict)
+        with open('filedata_google.json', 'w') as json_file:
+            for photo, content in info_dict.items():
+                metadata = {
+                    'name': content['filename'],
+                    'parents': [folder_id]
+                }
+                media = MediaFileUpload(content['filename'], mimetype='image/jpeg')
+                upload = service.files().create(body=metadata, media_body=media, fields='id').execute()
+                print(f'Файл {content["filename"]} загружен на Google Диск')
+                data[counter] = {'filename': content['filename'], 'size': content['size']}
+                counter += 1
+            json.dump(data, json_file, indent=4)
+        return info_dict
+
+    def upload_files(self, info_dict):
+        res = self._upload_to_gdrive(info_dict)
+        delete_files(res)
+
--- main.py
+++ main.py
@@ -1,14 +1,17 @@
+from gdrive import GDrive
 from yandex import Yandex
 from vkontakte import VK
-from ok import OK
-from datetime import datetime
+
+
 
 def main():
-    token_yandex = ''
+    # token_yandex = 'AQAAAAAOgRTpAADLW9eaCRyTskHLg71bc-vNziw'
     vkont = VK()
-    likes_and_url = vkont.get_photos('1', 'profile')
-    yand = Yandex(token_yandex)
-    yand.upload_urls(likes_and_url)
+    drive = GDrive()
+    likes_and_url = vkont.get_photos('552934290', 'profile')
+    drive.upload_files(likes_and_url)
+    # yand = Yandex(token_yandex)
+    # yand.upload_urls(likes_and_url)
 
 
 if __name__ == '__main__':
